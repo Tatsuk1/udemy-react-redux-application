201f33a39dde1cecdf6f66748794030fcfddea07
Tatsuk1 <t.tatsuki0417@gmail.com>
2019-12-23 15:46:11 +0900

    implement component with connect function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import{ createStore } from 'redux'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import './index.css';
 import reducer from './reducers'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducer)
 
 ReactDOM.render(
 	<Provider store={store}>
